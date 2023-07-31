0791270e806d8840f15851560f02e384a4c7ec6c
jpascoe32fb <74384275+jpascoe32fb@users.noreply.github.com>
2023-07-31 15:24:43 -0400

    Fixed attachments and Modal Popup
    Attachments pulled to be shown now have a max size to be rendered and scale down if necessary. They also link to themselves so that they can be further viewed/downloaded. The modals now have a backdrop again

--- orgs/models.py
+++ orgs/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import os
 # Create your models here.
 
 class Severity(models.Model):
@@ -32,11 +32,12 @@ class Condition(models.Model):
     technology = models.ForeignKey(Technology, null=True, on_delete=models.SET_NULL)
     analyst = models.ForeignKey(Analyst, null=True, on_delete=models.SET_NULL)
     entry_date = models.DateField(null=True)
+    data_collection_date = models.DateField(null=True)
     closed = models.BooleanField(null=True, default=False)
     close_date = models.DateField(null=True, blank=True)
-    work_req = models.CharField(max_length=300, null=True)
-    work_order = models.CharField(max_length=300, null=True)
-    reason = models.CharField(max_length=200, null=True, blank=True)
+    #work_req = models.CharField(max_length=300, null=True)
+    #work_order = models.CharField(max_length=300, null=True)
+    reason = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.severityLevel
@@ -44,21 +45,25 @@ class Condition(models.Model):
 
 class UnitName(models.Model):
     name = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True, blank=True)
     def __str__(self):
         return self.name
 
 class Function(models.Model):
     name = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True, blank=True)
     def __str__(self):
         return self.name
 
 class Asset(models.Model):
     name = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True, blank=True)
     def __str__(self):
         return self.name
 
 class Component(models.Model):
     name = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True, blank=True)
     def __str__(self):
         return self.name
 
@@ -70,10 +75,10 @@ class PlantTag(models.Model):
 
 class Unit(models.Model):
     name = models.ForeignKey(UnitName, null=True, on_delete=models.SET_NULL)
-    function = models.ForeignKey(Function, null=True, on_delete=models.SET_NULL)
-    asset = models.ForeignKey(Asset, null=True, on_delete=models.SET_NULL)
-    component = models.ForeignKey(Component, null=True, on_delete=models.SET_NULL)
-    plant_tag = models.ForeignKey(PlantTag, null=True, on_delete=models.SET_NULL)
+    function = models.ForeignKey(Function, null=True, blank=True, on_delete=models.SET_NULL)
+    asset = models.ForeignKey(Asset, null=True, blank=True, on_delete=models.SET_NULL)
+    component = models.OneToOneField(Component, null=True, blank=True, on_delete=models.SET_NULL)
+    plant_tag = models.ForeignKey(PlantTag, null=True, blank=True, on_delete=models.SET_NULL)
     #report = models.ForeignKey(Report, null=True, on_delete=models.SET_NULL)
     
     def __str__(self):
@@ -81,7 +86,8 @@ class Unit(models.Model):
 
 class FaultGroup(models.Model):
     fault = models.CharField(max_length=200, null=True, blank=True)
-    fault_group = models.CharField(max_length=200, null=True, blank=True)
+    fault_group = models.CharField(max_length=250, null=True, blank=True)
+    used_amount = models.PositiveIntegerField(null=True, blank=True, default=int(0))
 
     def __str__(self):
         return f"{self.fault}"
@@ -93,11 +99,26 @@ class Report(models.Model):
 
     fault = models.CharField(max_length=1000, null=True)
     fault_group = models.ManyToManyField(FaultGroup, blank=True)
-    comment = models.CharField(max_length=1000, null=True)
-    recommendation = models.CharField(max_length=1000, null=True)
+    comment = models.CharField(max_length=1200, null=True) #can be converted to textField(s)
+    recommendation = models.CharField(max_length=1200, null=True)
 
     def __str__(self):
         try:
             return f"{self.unit.name.name}  {self.condition.entry_date}"
         except:
             return f"Dead"
+        
+
+class Attachment(models.Model):
+    def file_directory_path(instance, filename):
+        _, extension = os.path.splitext(filename)
+        return 'uploads/{}/{}/{}/{}/{}'.format(
+            instance.report_instance.unit.name.name,
+            instance.report_instance.unit.function.name, 
+            instance.report_instance.unit.asset.name,
+            instance.report_instance.id, filename
+            )
+    
+    id = models.AutoField(primary_key=True)
+    report_instance = models.ForeignKey(Report, blank=True, null=True, on_delete=models.CASCADE)
+    file = models.FileField(upload_to= file_directory_path, blank=True, null=True)
